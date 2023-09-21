48b4da517e8c1bbf0b9a8b5500621b989069730d
ErikGear <erikgalindo23@aragon.unam.mx>
2023-09-21 17:46:32 -0600

    mutabilidad e inmutabilidad con objetos

--- inmutabilidad/main.js
+++ inmutabilidad/main.js
@@ -22,7 +22,50 @@ function mulInmutable(collection, num){
 
     return newNumbers;
 }
-
-
 const newArray2 = mulInmutable(numbers2,5);
 console.log(newArray2);
+
+
+//Inmutabilidad en Objetos
+class Beer {
+    constructor(name, price){
+        this._name = name;
+        this._price = price;
+    }
+
+    set name(name) {
+        this._name = name;
+    }
+    get name(){
+        return this._name;
+    }
+
+    set price(price){
+        this._price = price;
+    }
+    get name(){
+        return this._price;
+    }
+}
+
+//modifica el objeto original
+function toUpperMutable(beer){
+    beer.name = beer.name.toUpperCase();
+    return beer;
+}
+
+//no modifica el objeto original
+//realiza una copia del objeto
+function toUpperImutable(beer){
+    // de esta forma estamos creando una copia del objeto y asi no modificamos el objeto original
+   const newBeer = {...beer}; //operador spread
+   newBeer.name = newBeer.name.toUpperCase();
+   return newBeer;
+}
+
+const beer = new Beer("delirium", 6);
+const newBeer = toUpperMutable(beer);
+console.log(newBeer);
+console.log(beer);
+
+console.log(toUpperImutable(beer));
