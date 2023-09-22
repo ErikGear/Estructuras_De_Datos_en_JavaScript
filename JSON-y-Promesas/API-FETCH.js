16add8421abadd615bed64d726d7834f3a353ee8
ErikGear <erikgalindo23@aragon.unam.mx>
2023-09-21 19:40:38 -0600

    promesas con Async y Await

--- JSON-y-Promesas/API-FETCH.js
+++ JSON-y-Promesas/API-FETCH.js
@@ -8,3 +8,17 @@ que esta en algun servicio, endpoint, servicio en el backend
 fetch("https://jsonplaceholder.typicode.com/users")
     .then(data => data.json())
     .then(json => console.log(json));
+
+
+//async y await
+(function(){
+    console.log("Souy una funcion de expresion ejecutada inmediatamente");
+});
+
+( async function() {
+    //await solo puede ser empleada en una funcion con Async
+    //await, se esperara hasta que el proceso termine
+    const data = await fetch("https://jsonplaceholder.typicode.com/users");
+    const jsonResult = await data.json();
+    console.log(jsonResult);
+} );
