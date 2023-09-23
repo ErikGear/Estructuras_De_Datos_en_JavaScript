b1e822c82c79931eb03a7b741da142d739249f13
ErikGear <erikgalindo23@aragon.unam.mx>
2023-09-23 12:57:34 -0600

    accediendo a archivos JSON mediante FETCH

--- JSON-y-Promesas/API-FETCH.js
+++ JSON-y-Promesas/API-FETCH.js
@@ -1,24 +1,36 @@
 /*
-El API FETCH nos sirve par aobtener un recurso externo
+El API FETCH nos sirve para obtener un recurso externo
 que esta en algun servicio, endpoint, servicio en el backend
 */
 
 //API FETCH
 //esta api es una promesa ya hecha
 fetch("https://jsonplaceholder.typicode.com/users")
-    .then(data => data.json())
-    .then(json => console.log(json));
-
+  .then((data) => data.json())
+  .then((json) => console.log(json));
 
 //async y await
-(function(){
-    console.log("Souy una funcion de expresion ejecutada inmediatamente");
+(function () {
+  console.log("Souy una funcion de expresion ejecutada inmediatamente");
 });
 
-( async function() {
-    //await solo puede ser empleada en una funcion con Async
-    //await, se esperara hasta que el proceso termine
-    const data = await fetch("https://jsonplaceholder.typicode.com/users");
-    const jsonResult = await data.json();
-    console.log(jsonResult);
-} );
+(async function () {
+  //await solo puede ser empleada en una funcion con Async
+  //await, se esperara hasta que el proceso termine
+  const data = await fetch("https://jsonplaceholder.typicode.com/users");
+  const jsonResult = await data.json();
+  console.log(jsonResult);
+});
+
+//leyendo nuestro archivo JSON con el API fetch
+fetch("people.json")
+  .then((data) => data.json())
+  .then((json) => {
+    const people = json;
+
+    console.log(people.name);
+
+    for (const skill of people) {
+      console.log(`Habilidad: ${skill}`);
+    }
+  });
